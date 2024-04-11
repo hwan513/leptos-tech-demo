c30fbc653267ea1941d6bfefaa274c8a529323fe
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-04-11 22:49:50 +1200

    feat: add head tags with metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 use leptos::*;
+use leptos_meta::*;
 
 /// The application component
 #[component]
 pub fn App() -> impl IntoView {
+    provide_meta_context();
     view! {
+        <Html lang="en" dir="ltr" attr:data-theme="light"/>
+        // sets the document title
+        <Title text="Leptos Demo"/>
+        <Meta charset="UTF-8"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <div>
             <p>"Hello, world!"</p>
         </div>
