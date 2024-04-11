3806cd7484d6729efd087c82cd690411d186db7a
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-04-12 11:53:24 +1200

    docs(lab1): add documetation for about and light bulb

--- src/components/lab1/mod.rs
+++ src/components/lab1/mod.rs
@@ -1,13 +1,16 @@
 use leptos::*;
 
+/// This component will encapsulate all the features explored in lab 1
 #[component]
 pub fn Lab1() -> impl IntoView {
     view! {
         <h1>"Lab 1"</h1>
         <About/>
+        <LightBulb/>
     }
 }
 
+/// Usage of signals and prop is demonstrated here
 #[component]
 pub fn About() -> impl IntoView {
     let (name, setName) = create_signal(String::new());
@@ -20,30 +23,40 @@ pub fn About() -> impl IntoView {
             }
         />
 
-        <AboutMe name=name age=8/>
+        <AboutMe name=name age=21/>
     }
 }
 
 #[component]
 pub fn AboutMe(
+    /// This is a required component property, the code will not compile without it
+    /// You can also see how ReadSignal<> can be passed into props by defining the type
     name: ReadSignal<String>,
-    #[prop(optional)] age: u8,
-    #[prop(default="pizza".to_string())] favourite_food: String,
+    /// Optional property: defaults to the default value of the data type: 0 for u8
+    #[prop(optional)]
+    age: u8,
+    /// Optional property with default: if no value is given, then it will default to pizza
+    #[prop(default="pizza".to_string())]
+    favourite_food: String,
 ) -> impl IntoView {
     view! {
-        <p>"Hello " {name} " welcome to Leptos Demo!"</p>
+        <p>"Hello " {name} "and welcome to Leptos Demo!"</p>
         <p>"My name is Henry. I'm " {age} " years old, and I like " {favourite_food} "."</p>
     }
 }
 
+/// Usage of using if/else for control flow is demonstrated here
 #[component]
 pub fn LightBulb() -> impl IntoView {
     let (is_on, set_on) = create_signal(false);
+    // Closures can also be set to variables which can then be called later on with on:click
     let toggle_light = move |_| {
         set_on(!is_on.get());
     };
     view! {
         <img
+            // To keep reactitivity, the if/else control flow needs to enclosed by a closure:
+            // move || if {} else {}
             src=move || if is_on() { "images/light-on.png" } else { "images/light-off.png" }
             on:click=toggle_light
         />
